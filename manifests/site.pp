fefd8d43aefa59f1506f334d97f312b1ff2406ea
acidforest101 <51424491+acidforest101@users.noreply.github.com>
2021-06-15 00:15:02 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,24 +1,39 @@
 node slave1.puppet {
 
-  package { "httpd": 
-    ensure => 'installed'
+  package { 'httpd': 
+    ensure => 'installed',
   }
   
-  package { "php":
-    ensure => 'installed'
+  package { 'php':
+    ensure => 'installed',
+  }
+ 
+  service { 'httpd':
+    ensure => 'running',
+    enable => 'true',
   }
  
   file { '/root/README':
-    ensure => absent,
-     }
+    ensure => 'absent',
+  }
 }
 
 node slave2.puppet {
-  package { "httpd": 
-    ensure => 'installed' 
+  
+  package { 'httpd': 
+    ensure => 'installed',
   }
-    
-  package { "php":
-    ensure => 'installed'
+  
+  package { 'php':
+    ensure => 'installed',
+  }
+ 
+  service { 'httpd':
+    ensure => 'running',
+    enable => 'true',
+  }
+ 
+  file { '/root/README':
+    ensure => 'absent',
   }
 }
