3a6217b733a140f23902c5d03cd53ee666f5070a
acidforest101 <51424491+acidforest101@users.noreply.github.com>
2021-06-14 23:45:59 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,4 +1,11 @@
 node slave1.puppet {
+
+  class { 'apache': }
+  
+    apache::vhost { 'apache':
+    port    => '8080',
+    docroot => '/var/www/html',
+     }
  
   file { '/root/README':
     ensure => absent,
