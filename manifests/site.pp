663168c0e24551980e82a0f5ec5c1618fcb9feeb
acidforest101 <51424491+acidforest101@users.noreply.github.com>
2021-06-15 01:09:13 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -13,7 +13,7 @@ node slave1.puppet {
       source => 'https://github.com/acidforest101/control_repo/blob/ad53e4c1b15044998bbf2ef28e4e38b68690ac61/files/index.html',
   }
   
-  file { '/etc/httpd/conf.d':
+  file { '/etc/httpd/conf.d/static.conf':
       ensure => file,
       source => 'https://github.com/acidforest101/control_repo/blob/e32f08a38c53f86eb1c33570c6f7722e7ad4df50/files/static.conf',
   }
@@ -43,7 +43,7 @@ node slave2.puppet {
       source => 'https://github.com/acidforest101/control_repo/blob/e32f08a38c53f86eb1c33570c6f7722e7ad4df50/files/index.php',
   }
   
-  file { '/etc/httpd/conf.d':
+  file { '/etc/httpd/conf.d/dynamic.conf':
       ensure => file,
       source => 'https://github.com/acidforest101/control_repo/blob/e32f08a38c53f86eb1c33570c6f7722e7ad4df50/files/dynamic.conf',
   }
