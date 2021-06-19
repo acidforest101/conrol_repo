8d0295da720052ae7eda7a61565cd5905e66d9f3
acidforest101 <51424491+acidforest101@users.noreply.github.com>
2021-06-19 22:55:09 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -69,4 +69,12 @@ node mineserver.puppet{
     source => 'https://launcher.mojang.com/v1/objects/0a269b5f2c5b93b1712d0f5dc43b6182b9ab254e/server.jar',
     replace => false,
   }
+  
+  systemd::unit_file { 'mineserver.service':
+    source => "puppet:///modules/mineserver.service",
+  }
+  
+  service {'mineserver':
+    ensure => 'running',
+  }
 }
