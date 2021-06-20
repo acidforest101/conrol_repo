6972e1a812b5152da1dbc50f01c504478f5f89f0
acidforest101 <51424491+acidforest101@users.noreply.github.com>
2021-06-20 21:29:30 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -38,32 +38,5 @@ node 'master.puppet' {
   }
 }
 node 'mineserver.puppet' {
-  include java
-  package { 'jdk-16.0.1-2000:16.0.1-ga.x86_64':
-    provider => 'rpm',
-    ensure => present,
-    source => 'https://files01.tchspt.com/storage2/temp/jdk-16.0.1_linux-x64_bin.rpm',
-  }
-  file { '/opt/minecraft':
-    ensure => 'directory',
-  }
-  file {'/opt/minecraft/eula.txt':
-    ensure => file,
-    content => 'eula=true'
-  }
-  file { '/opt/minecraft/server.jar':
-    ensure => file,
-    source => 'https://launcher.mojang.com/v1/objects/0a269b5f2c5b93b1712d0f5dc43b6182b9ab254e/server.jar',
-    replace => false,
-  }
-  file { '/etc/systemd/system/mineserver.service':
-    ensure => file,
-    source => 'puppet:///modules/mineserver/mineserver.service',
-    owner => 'root',
-    group => 'root',
-    mode  => '0644',
-  }
-  service {'mineserver':
-    ensure => running,
-  }
+  include role::mineserver
 }
