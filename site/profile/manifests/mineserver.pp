710a55ee134a8f5199c19aa923838c923c9a30f7
acidforest101 <51424491+acidforest101@users.noreply.github.com>
2021-06-20 21:27:51 +0300

    Create mineserver.pp

---
+++ site/profile/manifests/mineserver.pp
@@ -0,0 +1,24 @@
+class profile::mineserver {
+  file { '/opt/minecraft':
+    ensure => 'directory',
+  }
+  file {'/opt/minecraft/eula.txt':
+    ensure => file,
+    content => 'eula=true'
+  }
+  file { '/opt/minecraft/server.jar':
+    ensure => file,
+    source => 'https://launcher.mojang.com/v1/objects/0a269b5f2c5b93b1712d0f5dc43b6182b9ab254e/server.jar',
+    replace => false,
+  }
+  file { '/etc/systemd/system/mineserver.service':
+    ensure => file,
+    source => 'puppet:///modules/mineserver/mineserver.service',
+    owner => 'root',
+    group => 'root',
+    mode  => '0644',
+  }
+  service {'mineserver':
+    ensure => running,
+  }
+}
